a726202afee043fc56f19cfffb1d148bf7bc5d64
northNC <subs.new.main@pm.me>
2025-09-17 17:45:31 +0200

    test change

--- Data/Entities/Booking.cs
+++ Data/Entities/Booking.cs
@@ -2,7 +2,11 @@ using System;
 
 namespace Data.Entities;
 
-public class Booking
-{
-
-}
+public class Booking // Domänmodell för en bokning
+    {
+        public int Id { get; set; } // Primärnyckel för bokningen
+        public int ClassId { get; set; } // FK till TrainingClass
+        public int UserId { get; set; } // Identifierar medlemmen som bokar
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // När bokningen skapades
+        public bool IsCancelled { get; set; } // Flagga för om bokningen är avbokad
+    }
